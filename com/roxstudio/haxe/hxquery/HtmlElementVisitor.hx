6477082e437bba548eb76297af279425b9f274bf
yanhick <yannick.dominguez@gmail.com>
2013-05-21 11:27:11 +0200

    implemented as Haxe2

--- com/roxstudio/haxe/hxquery/HtmlElementVisitor.hx
+++ com/roxstudio/haxe/hxquery/HtmlElementVisitor.hx
@@ -1,50 +1,50 @@
 package com.roxstudio.haxe.hxquery;
 
 import com.roxstudio.haxe.hxquery.HxQuery;
-import js.html.HtmlElement;
-import js.html.Node;
+import js.Dom;
 using StringTools;
 
-class HtmlElementVisitor extends AbstractVisitor<HtmlElement> {
+class HtmlElementVisitor extends AbstractVisitor<HtmlDom> {
 
+	static inline var ELEMENT_NODE:Int = 1;
+	
     public function new() {
         super();
     }
 
-    override public function parent(n: HtmlElement) {
-        return cast n.parentNode;
+    override public function parent(n: HtmlDom) {
+        return n.parentNode;
     }
 
-    override public function typeOf(n: HtmlElement) : String {
+    override public function typeOf(n: HtmlDom) : String {
 		//html is case-insensitive
-        return n.nodeType == Node.ELEMENT_NODE ? n.nodeName.toLowerCase() : "";
+        return n.nodeType == ELEMENT_NODE ? n.nodeName.toLowerCase() : "";
     }
 
-    override public function idOf(n: HtmlElement) : String {
-        return n.nodeType == Node.ELEMENT_NODE ? n.id : "";
+    override public function idOf(n: HtmlDom) : String {
+        return n.nodeType == ELEMENT_NODE ? n.id : "";
     }
 
-    override public function hasClass(n: HtmlElement, className: String) : Bool {
-        var clz = n.nodeType == Node.ELEMENT_NODE ? n.className : null;
+    override public function hasClass(n: HtmlDom, className: String) : Bool {
+        var clz = n.nodeType == ELEMENT_NODE ? n.className : null;
         return clz != null ? HxQuery.inArray(className, clz.split(" ")) : false;
     }
 
-    override public function hasAttribute(n: HtmlElement, name: String) {
-        return n.nodeType == Node.ELEMENT_NODE ? n.getAttribute(name) == null : false;
+    override public function hasAttribute(n: HtmlDom, name: String) {
+        return n.nodeType == ELEMENT_NODE ? n.getAttribute(name) == null : false;
     }
 
-    override public function attribute(n: HtmlElement, name: String) : Dynamic {
-        return n.nodeType == Node.ELEMENT_NODE ? (name == "__value__" ? n.nodeValue : n.getAttribute(name)) : null;
+    override public function attribute(n: HtmlDom, name: String) : Dynamic {
+        return n.nodeType == ELEMENT_NODE ? (name == "__value__" ? n.nodeValue : n.getAttribute(name)) : null;
     }
 
-    override public function setAttribute(n: HtmlElement, name: String, value: Dynamic) : Void {
-        if (n.nodeType == Node.ELEMENT_NODE) n.setAttribute(name, value.toString());
+    override public function setAttribute(n: HtmlDom, name: String, value: Dynamic) : Void {
+        if (n.nodeType == ELEMENT_NODE) n.setAttribute(name, value.toString());
     }
 
-    override public function children(n: HtmlElement) : Array<HtmlElement> {
-        var result: Array<HtmlElement> = [];
-		//cast as childNodes are of type Node
-        for (e in n.childNodes) result.push(cast e);
+    override public function children(n: HtmlDom) : Array<HtmlDom> {
+        var result: Array<HtmlDom> = [];
+        for (i in 0...n.childNodes.length) result.push(n.childNodes[i]);
         return result;
     }
 
